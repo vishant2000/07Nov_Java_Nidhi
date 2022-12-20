c6004c78125730ea4bceb7be601ce9c62a7f36f0
Vishant Dalsaniya <113351811+vishant2000@users.noreply.github.com>
2022-12-20 16:31:45 +0530

    Create calcusingswitch.c

---
+++ Assignment/Module-3/Module-3.2/calcusingswitch.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+void main()
+{
+    int no1,no2;
+    int choice;
+    float ans;
+    printf("Enter the value of two numbers : ");
+    scanf("%d%d",&no1,&no2);
+    printf("\n 1=addition \n 2=substraction \n 3=division \n 4=multiplication \n 5=modulo");
+    printf("\nEnter the choice to perform the operation : ");
+    scanf("%d",&choice);
+    
+    switch(choice)
+ {
+    case 1:
+          ans=no1+no2;
+          printf("Addition of two numbers is = %f",ans);
+          break;
+          
+    case 2:
+          ans=no1-no2;
+          printf("Substraction of two numbers is = %f",ans);
+          break;
+          
+    case 3:
+          ans=no1/no2;
+          printf("Division of two numbers is = %f",ans);
+          break;
+          
+    case 4:
+          ans=no1*no2;
+          printf("Multiplication of two numbers is = %f",ans);
+          break;
+          
+    case 5:
+          ans=no1%no2;
+          printf("Modulo of two numbers is = %f",ans);
+          break;
+          
+    default:
+          printf("Enter valid choice");
+          break;
+ }
+ 
+}
