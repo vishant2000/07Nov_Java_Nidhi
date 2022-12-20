02d52d9787d4ebd8391f34b621d59e9c5e7407c8
Vishant Dalsaniya <113351811+vishant2000@users.noreply.github.com>
2022-12-20 15:48:48 +0530

    Create Ternery.c

---
+++ Assignment/Module-3/Module-3.2/Ternery.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+  int no;
+  printf("Enter the value of no : ");
+  scanf("%d",&no);
+  
+  (no%2==0) ? (printf("Your no is even")) : (printf("Your no is odd"));
+}
