dd55087e0e89831e7bb9a7a596ca4e677a820779
Vishant Dalsaniya <113351811+vishant2000@users.noreply.github.com>
2022-12-28 16:54:13 +0530

    Update Swap.c

--- Assignment/Module-3/Module-3.2/Swap.c
+++ Assignment/Module-3/Module-3.2/Swap.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+void main()
+{
+    int a;
+    int b;
+    int *ptr;
+    ptr = &a;
+    
+    printf("Enter the value of a = ");
+    scanf("%d",&a);
+    
+    printf("Enter the value of b = ");
+    scanf("%d",&b);
+    
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    
+    printf("\nNew value of a is %d",a);
+    printf("\nNew value of b is %d",b);
+}
