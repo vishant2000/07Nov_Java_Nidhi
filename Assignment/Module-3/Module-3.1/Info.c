02f89c724b19a672507642c72e5177603970d59b
Vishant Dalsaniya <113351811+vishant2000@users.noreply.github.com>
2022-12-28 15:33:11 +0530

    Create Info.c

---
+++ Assignment/Module-3/Module-3.1/Info.c
@@ -0,0 +1,15 @@
+/* WAP to display this information using printf 
+1. Your Name 
+2. Your Birth date
+3. Your Age 
+4. Your Address*/
+
+ #include<stdio.h>
+   void main()
+  {
+	  printf("Name : Vishant \n");
+	  printf("DOB : 10/12/2000 \n");
+	  printf("AGE : 21");
+    printf("Address : 80 Whitehorn Crescent, North York, ON M2J 3B6, Canada");
+
+  }
