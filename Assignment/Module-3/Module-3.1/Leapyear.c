b3548ae4dc54a9240b31a9f8c7fc23271db112d5
Vishant Dalsaniya <113351811+vishant2000@users.noreply.github.com>
2022-12-28 15:48:20 +0530

    Create Leapyear.c

---
+++ Assignment/Module-3/Module-3.1/Leapyear.c
@@ -0,0 +1,17 @@
+//WAP to check if given number is leap year or not
+
+#include <stdio.h>
+ void main() 
+{
+   int year;
+   printf("Enter a year: ");
+   scanf("%d", &year);
+   
+   if (year % 4 == 0) 
+   {
+      printf("%d is a leap year.",year);
+   }
+   else {
+      printf("%d is not a leap year.",year);
+   }
+}
