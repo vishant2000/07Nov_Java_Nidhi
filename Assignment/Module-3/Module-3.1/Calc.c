93bee8a16bd8418253fc332bc47d52f5a7f575a6
Vishant Dalsaniya <113351811+vishant2000@users.noreply.github.com>
2022-12-28 15:37:15 +0530

    Create Calc.c

---
+++ Assignment/Module-3/Module-3.1/Calc.c
@@ -0,0 +1,20 @@
+/* Write a program to make Simple calculator (to make addition, subtraction, 
+multiplication, division and modulo) */					       
+							       
+#include<stdio.h>
+ void main ()
+ {
+	  int A ;
+	  int B ;
+	
+	  printf("Enter the value of A=");
+	  scanf("%d",&A);
+	  printf("Enter the value of B=");
+	  scanf("%d",&B);
+   
+	  printf("A + B = %d \n",A+B);
+	  printf("A - B = %d \n",A-B);
+    printf("A * B = %d \n",A*B);
+	  printf("A / B = %d \n",A/B);
+    printf("A % B = %d \n",A%B);	
+ }
